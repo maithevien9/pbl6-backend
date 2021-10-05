77e5c648da51eb63f20211bcd83df39fca664ceb
maithevien9 <maithevien9@gmail.com>
2021-10-06 01:28:07 +0700

    add middleware authorized

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,41 +1,41 @@
-// import { RequestHandler } from 'express';
-// import httpStatus from 'http-status';
+import { RequestHandler } from 'express';
+import httpStatus from 'http-status';
 
-// import { User } from '../models';
-// import JWT from '../utils/jwt';
-// import APIError from '../utils/APIError';
+import { User } from '../models';
+import JWT from '../utils/jwt';
+import APIError from '../utils/APIError';
 
-// class AuthMiddleware {
-//   static requireAuth: RequestHandler = async (req, res, next) => {
-//     try {
-//       const token = JWT.get(req);
+class AuthMiddleware {
+  static requireAuth: RequestHandler = async (req, res, next) => {
+    try {
+      const token = JWT.get(req);
 
-//       if (!token) {
-//         throw new APIError({
-//           status: httpStatus.UNAUTHORIZED,
-//           message: 'Unauthorized',
-//         });
-//       }
+      if (!token) {
+        throw new APIError({
+          status: httpStatus.UNAUTHORIZED,
+          message: 'Unauthorized',
+        });
+      }
 
-//       const tokenPayload = JWT.verify(token);
-//       const user = await User.findOne({
-//         _id: tokenPayload._id,
-//         isBlocked: false,
-//       });
+      const tokenPayload = JWT.verify(token);
+      const user = await User.findOne({
+        _id: tokenPayload._id,
+        isVerify: true,
+      });
 
-//       if (!user) {
-//         throw new APIError({
-//           status: httpStatus.NOT_FOUND,
-//           message: 'User not found',
-//         });
-//       }
+      if (!user) {
+        throw new APIError({
+          status: httpStatus.NOT_FOUND,
+          message: 'User not found',
+        });
+      }
 
-//       req.user = user;
-//       next();
-//     } catch (e) {
-//       next(e);
-//     }
-//   };
-// }
+      req.user = user;
+      next();
+    } catch (e) {
+      next(e);
+    }
+  };
+}
 
-// export default AuthMiddleware;
+export default AuthMiddleware;
--- src/routes/v1/role.route.ts
+++ src/routes/v1/role.route.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import { RoleController } from '../../controllers';
-// import AuthMiddleware from '../../middlewares/auth.middleware';
+import AuthMiddleware from '../../middlewares/auth.middleware';
 
 const router = express.Router();
 
-router.route('/').get(RoleController.getAll);
+router.route('/').get([AuthMiddleware.requireAuth], RoleController.getAll);
 
 export default router;
