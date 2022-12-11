c569e2cc83d825c7b8aa119ddd428aabe3adf52b
maithevien9 <vienthe.mai@team.enouvo.com>
2022-12-11 21:04:03 +0700

    Remove middleware

--- src/routes/v1/upload.route.ts
+++ src/routes/v1/upload.route.ts
@@ -1,21 +1,15 @@
 import express from 'express';
 
 import { UploadController } from '../../controllers';
-import { AuthMiddleware, MulterMiddleware } from '../../middlewares';
+import { MulterMiddleware } from '../../middlewares';
 
 const router = express.Router();
 
 router
   .route('/single')
-  .post(
-    [AuthMiddleware.requireAuth, MulterMiddleware.upload.single('photo')],
-    UploadController.single,
-  );
+  .post([MulterMiddleware.upload.single('photo')], UploadController.single);
 router
   .route('/multiple')
-  .post(
-    [AuthMiddleware.requireAuth, MulterMiddleware.upload.array('photo')],
-    UploadController.multiple,
-  );
+  .post([MulterMiddleware.upload.array('photo')], UploadController.multiple);
 
 export default router;
